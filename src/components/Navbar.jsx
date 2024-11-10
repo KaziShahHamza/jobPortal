f93ea5b66e4729eef96cee619cad0feb315c70ae
soulfulDreamer <kazishahhamza@outlook.com>
2024-11-10 11:23:04 +0600

    Hero Section Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/images/logo3.png"
+
 const Navbar = () => {
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -11,11 +13,11 @@ const Navbar = () => {
             >
               <img
                 className="h-10 w-auto"
-                src="images/logo.png"
+                src={ logo }
                 alt="React Jobs"
               />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                React Jobs
+                NextJob
               </span>
             </a>
             <div className="md:ml-auto">
