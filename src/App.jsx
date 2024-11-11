20e186d1b0e6234b1b9c036a27e0f60c2d868a1b
soulfulDreamer <kazishahhamza@outlook.com>
2024-11-11 22:09:01 +0600

    routing stated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import Navbar from "./components/Navbar";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 import "./index.css";
 
-function App() {
-  return <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-  </>;
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
